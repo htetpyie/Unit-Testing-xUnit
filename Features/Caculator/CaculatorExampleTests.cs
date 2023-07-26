24d7dc74943ad08fceaa32eeecb777192070eb9f
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-07-26 22:40:56 +0630

    member data and inline data

--- Features/Caculator/CaculatorExampleTests.cs
+++ Features/Caculator/CaculatorExampleTests.cs
@@ -23,5 +23,36 @@ namespace xUnitTesting.Features.Caculator
             //Assert
             Assert.Equal(expectedResult, actualResult);
         }
+
+        //Inline Data
+        [Theory]
+        [InlineData(-1, -3, -4)]
+        public void Add_NegativeNumbers_ReturnsExpectedResult(int a, int b, int expectedResult)
+        {
+            int actualResult = new CaculatorExample().Add(a, b);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Member Data
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { 1, 2, 3 };
+                yield return new object[] { -1, -2, -3 };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Add_Numbers_ReturnsExpectedResult(int a, int b, int expectedResult)
+        {
+            int actualResult = new CaculatorExample().Add(a, b);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
+
 }
