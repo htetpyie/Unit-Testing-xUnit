39c1298a0f6ca6bd38ede647707ae70027154712
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-07-26 22:20:11 +0630

    added readme.md and caculator

---
+++ Features/Caculator/CaculatorExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTesting.Features.Caculator
+{
+    public class CaculatorExample
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Features/Caculator/CaculatorExampleTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTesting.Features.Caculator
+{
+    public class CaculatorTests
+    {
+    }
+}
