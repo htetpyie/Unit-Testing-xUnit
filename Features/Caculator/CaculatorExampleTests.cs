833f91bb7ff1519b9d408f29865051815bb354db
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-07-26 22:27:08 +0630

    test add method

--- Features/Caculator/CaculatorExampleTests.cs
+++ Features/Caculator/CaculatorExampleTests.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace xUnitTesting.Features.Caculator
 {
-    public class CaculatorTests
+    public class CaculatorExampleTests
     {
+        [Fact]
+        public void Add_PositiveNumbers_ReturnsExpectedResult()
+        {
+            //Arrange 
+            var caculator = new CaculatorExample();
+            int a = 3;
+            int b = 4;
+            int expectedResult = 7;
+
+            //Act
+            int actualResult = caculator.Add(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
