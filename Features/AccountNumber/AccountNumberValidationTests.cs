559c371f055fd531cc482ef3e89aaa20e9959549
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-08-01 23:28:42 +0630

    added argument exception

--- Features/AccountNumber/AccountNumberValidationTests.cs
+++ Features/AccountNumber/AccountNumberValidationTests.cs
@@ -18,13 +18,33 @@ namespace xUnitTesting.Features.AccountNumber
         public void IsValid_ValidAccountNumber_ReturnsTrue()
             => Assert.True(_validation.IsValid("123-4543234576-23"));
 
-        [Fact]
-        public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse()
-           => Assert.False(_validation.IsValid("1234-3454565676-23"));
+        //[Fact]
+        //public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse()
+        //   => Assert.False(_validation.IsValid("1234-3454565676-23"));
 
         [Theory]
-        [InlineData("")]
+        [InlineData("1234-3454565676-23")]
+        [InlineData("12-3454565676-23")]
         public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse(string accountNumber)
             => Assert.False(_validation.IsValid(accountNumber));
+
+        [Theory]
+        [InlineData("123-345456567-23")]
+        [InlineData("123-345456567633-23")]
+        public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-3434545656-2")]
+        [InlineData("123-3454565676-233")]
+        public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-345456567633=23")]
+        [InlineData("123+345456567633-23")]
+        [InlineData("123+345456567633=23")]
+        public void IsValid_InvalidDelimiters_ThrowsArgumentException(string accNumber)
+            => Assert.Throws<ArgumentException>(() => _validation.IsValid(accNumber));
     }
 }
