1b6713e125067f412c1867bbbcb5293949f68469
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-08-01 23:16:42 +0630

    added is valid test

---
+++ Features/AccountNumber/AccountNumberValidationTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTesting.Features.AccountNumber
+{
+    public class AccountNumberValidationTests
+    {
+        AccountNumberValidation _validation = new AccountNumberValidation();
+
+        public AccountNumberValidationTests() => 
+            _validation = new AccountNumberValidation();
+
+        public void IsValid_ValidAccountNumber_ReturnsTrue()
+            => Assert.True(_validation.IsValid("123-4543234576-23"));
+    }
+}
